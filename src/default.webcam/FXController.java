fad05ef00ce970f855069d550d64f2575e6d0ca8
myles p <Myles Pasetsky>
2020-01-27 00:17:17 -0500

    Initial commit. Add default project.

---
+++ src/default.webcam/FXController.java
@@ -0,0 +1,104 @@
+package cv;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import utils.Utils;
+
+public class FXController {
+
+	@FXML
+	private Button button;
+	@FXML
+	private ImageView currentFrame;
+	
+	private ScheduledExecutorService timer;
+	private VideoCapture capture;
+	private boolean cameraActive;
+	private static final int cameraId = 0;
+	
+	protected void init() {
+		capture = new VideoCapture();
+		cameraActive = false;
+		
+	}
+	
+	@FXML
+	protected void startCamera(ActionEvent event) {
+		if (!cameraActive) {
+			capture.open(cameraId);
+			if (capture.isOpened()) {
+				cameraActive = true;
+				
+				Runnable frameGrabber = () -> {
+					Mat frame = grabFrame();
+					Image imageToShow = Utils.mat2Image(frame);
+					updateImageView(currentFrame, imageToShow);
+				};
+				
+				timer = Executors.newSingleThreadScheduledExecutor();
+				timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
+				
+				button.setText("Stop Camera");
+			}
+			else {
+				System.err.println("Impossible to open the camera connection...");
+			}
+		}
+		else {
+			cameraActive = false;
+			button.setText("Start Camera");
+			
+			stopAcquisition();
+		}
+	}
+	
+	private Mat grabFrame() {
+		Mat frame = new Mat();
+		
+		if (capture.isOpened()) {
+			try {
+				capture.read(frame);
+			}
+			catch (Exception e) {
+				System.err.println("Exception during the image elaboration: " + e);
+			}
+		}
+		
+		return frame;
+	}
+	
+	private void stopAcquisition() {
+		if (timer!=null && !timer.isShutdown()) {
+			try {
+				timer.shutdown();
+				timer.awaitTermination(33, TimeUnit.MILLISECONDS);
+			}
+			catch (InterruptedException e) {
+				System.err.println("Exception in stopping the frame capture, trying to release the camera now... " + e);
+			}
+		}
+		
+		if (capture.isOpened()) {
+			capture.release();
+		}
+	}
+	
+	private void updateImageView(ImageView view, Image image) {
+		Utils.onFXThread(view.imageProperty(), image);
+	}
+	
+	protected void setClosed() {
+		stopAcquisition();
+	}
+	
+}
---
+++ src/default.webcam/Main.java
@@ -0,0 +1,44 @@
+package cv;
+// --module-path "C:\Program Files\Java Libraries\javafx\lib" --add-modules javafx.controls,javafx.fxml
+
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("MainFXML.fxml"));
+			BorderPane rootElement = (BorderPane) loader.load();
+			Scene scene = new Scene(rootElement, 800, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("WebCam");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			FXController controller = loader.getController();
+			controller.init();
+			
+			primaryStage.setOnCloseRequest((WindowEvent we) -> {
+				controller.setClosed();
+				System.exit(1);
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		launch(args);
+	}
+}
